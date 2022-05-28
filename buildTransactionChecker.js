d0d91d39c32e439a87e997cfe955535426807957
Hyeongwon-up <leehwdatabase@gmail.com>
2022-05-28 20:57:49 +0900

    feat: 수정

--- buildTransactionChecker.js
+++ buildTransactionChecker.js
@@ -2,32 +2,36 @@ const Web3 = require("web3");
 
 async function main() {
     // Configuring the connection to an Ethereum node
-    const network = process.env.ETHEREUM_NETWORK;
     const web3 = new Web3(
       new Web3.providers.HttpProvider(
         `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
       )
     );
         
-    var account = '0x26704Dc20d0ddF6cAa45b4D2b8AcB643015B951E'
-
+    var account = YOUR_ADDRESS
     let block = await web3.eth.getBlock('latest');
-
-    console.log(block)
 
     console.log("Searching Block ..");
         if(block && block.transactions) {
             for(let txHash of block.transactions) {
-                console.log(txHash)
                 let tx = await web3.eth.getTransaction(txHash);
-                if(account === tx.to.toLowerCase()) {
-                    console.log(`Transaction Foun of Block ${lastBLockNumber}`)
+            
+                if(account === tx.from) {
+                    console.log("트랜잭션 정보 획득!!")
                     console.log({
                         address: tx.from,
                         value: web3.utils.fromWei(tx.value, 'ether'),
                         timestamp: new Date()
                     });
                 }
+                if(account === tx.to) {
+                  console.log("트랜잭션 정보 획득!!")
+                  console.log({
+                      address: tx.from,
+                      value: web3.utils.fromWei(tx.value, 'ether'),
+                      timestamp: new Date()
+                  });
+              }
             }
         
   }
@@ -38,5 +42,5 @@ async function main() {
 
   setInterval(() => {
       main()
-  }, 7000)
+  }, 3000)
 
